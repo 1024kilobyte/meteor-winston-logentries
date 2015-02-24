2ac362316cbcf6a5b3d7ac96162fbaa4953f551e
1024kilobyte <mb@bytelog.de>
2015-02-24 15:47:19 +0100

    Version update for autopublish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": 'A Winston Logentries transport for Meteor.',
-  "version": "0.0.6",
+  "version": "0.0.7",
   "git": "https://github.com/1024kilobyte/meteor-winston-logentries.git",
   "name": "1024kilobyte:winston-logentries"
 });
@@ -11,10 +11,10 @@ Npm.depends({
 });
 
 Package.onUse(function (api, where) {
-  
+
   api.addFiles('winston.js', 'server');
   api.addFiles('winston-logentries.js', 'server');
-  
+
   if(api.export){
     api.export('Winston');
     api.export('Winston_Logentries');
